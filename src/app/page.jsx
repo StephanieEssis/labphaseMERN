d87defceedaf97366c321c966aeb5e9176f966d7
StephanieEssis <stephanieessis@gmail.com>
2025-05-14 00:11:45 +0000

    fix:img

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,9 +6,15 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <div className="p-8">
-        <h1 className="text-3xl font-bold text-center">Bienvenue à Mylan Lodge</h1>
-        <p className="mt-4 text-center">Découvrez nos chambres et réservez votre séjour.</p>
+      <div
+        className="flex flex-col items-center justify-center text-white p-8 min-h-[80vh] bg-cover bg-center"
+        style={{
+          backgroundImage:
+            "url('https://img.freepik.com/photos-gratuite/suite-luxe-dans-hotel-grande-hauteur-table-travail_105762-1783.jpg?ga=GA1.1.1335567724.1747181258&semt=ais_hybrid&w=740')",
+        }}
+      >
+        <h1 className="text-4xl font-bold text-center drop-shadow-lg">Bienvenue à Mylan Lodge</h1>
+        <p className="mt-4 text-center text-lg drop-shadow-md">Découvrez nos chambres et réservez votre séjour.</p>
       </div>
       <Footer />
     </>
